12378827cf37e4d82ca19dc39e218c2d73ad21b7
Lee Jeong <dlwjd8023@gmail.com>
2026-02-24 02:43:52 +0900

    Update 222－풀링.java

--- Java/백준/Silver/17829. 222－풀링/222－풀링.java
+++ Java/백준/Silver/17829. 222－풀링/222－풀링.java
@@ -4,43 +4,54 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Scanner stdIn = new Scanner(System.in);
-        
+
         int maxLength = stdIn.nextInt();
         int[][] array = new int[maxLength][maxLength];
-        
+
         for (int i = 0; i < maxLength; i++) {
             for (int j = 0; j < maxLength; j++) {
                 array[i][j] = stdIn.nextInt();
             }
         }
-       
+
         System.out.println(pulling(array, maxLength));
     }
-    
+
     static int secondMax(int a, int b, int c, int d) {
         int[] arr = {a, b, c, d};
         Arrays.sort(arr);
         return arr[2];
     }
-    
-    static int pulling(int[][] array, int length) {
-        if (length == 1) {
-            return array[0][0];
-        }
-        
-        int half = length / 2;
-        int[][] newArray = new int[half][half];
-        
-        for (int i = 0; i < half; i++) {
-            for (int j = 0; j < half; j++) {
-                int a = array[i * 2][j * 2];
-                int b = array[i * 2][j * 2 + 1];
-                int c = array[i * 2 + 1][j * 2];
-                int d = array[i * 2 + 1][j * 2 + 1];
-                newArray[i][j] = secondMax(a, b, c, d);
+
+    static int[][] extract(int[][] array, int rowStart, int rowEnd, int colStart, int colEnd) {
+        int[][] result = new int[rowEnd - rowStart][colEnd - colStart];
+        for (int i = rowStart; i < rowEnd; i++) {
+            for (int j = colStart; j < colEnd; j++) {
+                result[i - rowStart][j - colStart] = array[i][j];
             }
         }
-        
-        return pulling(newArray, half);
+        return result;
     }
+
+    static int pulling(int[][] array, int length) {
+        if (length == 2) {
+            return secondMax(array[0][0], array[0][1], array[1][0], array[1][1]);
+        }
+
+        int half = length / 2;
+
+        int[][] array1 = extract(array, 0,    half,   0,    half); 
+        int[][] array2 = extract(array, 0,    half,   half, length); 
+        int[][] array3 = extract(array, half, length, 0,    half);
+        int[][] array4 = extract(array, half, length, half, length); 
+
+        int v1 = pulling(array1, half);
+        int v2 = pulling(array2, half);
+        int v3 = pulling(array3, half);
+        int v4 = pulling(array4, half);
+
+        return secondMax(v1, v2, v3, v4);
+    }
+}
+
 }
