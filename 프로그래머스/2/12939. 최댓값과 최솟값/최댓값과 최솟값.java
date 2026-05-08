1c405b8f67366539f9748672c8660294553c1165
Lee Jeong <dlwjd8023@gmail.com>
2026-05-08 10:09:57 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 8.10 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        int[] arr = Arrays.stream(s.split(" "))
+            .mapToInt(Integer::parseInt)
+            .toArray();
+        
+        int max = Arrays.stream(arr).max().getAsInt();
+        int min = Arrays.stream(arr).min().getAsInt();
+        
+        return min + " " + max;
+    }
+}
